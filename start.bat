920f6e988371d1dc70f4fa4930a6191cfc60ba17
vip4ik <vk.vip@list.ru>
2025-09-04 08:13:46 +0300

    Add files via upload

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-start "Backend" cmd /k "cd C:\Users\NACHISTO\anon_chat\backend && uvicorn server:app --host 192.168.0.102 --port 8000 --reload"
+start "Backend" cmd /k "cd C:\Users\NACHISTO\anon_chat\backend && uvicorn server:app --reload --host 192.168.0.102 --port 8000"
 start "Frontend" cmd /k "cd C:\Users\NACHISTO\anon_chat\frontend && npm start"
-python C:\Users\NACHISTO\anon_chat\monitoring.py 
+ 
